526ad88467f26b2f8a38380728657006a411b258
slemeshaev <lemeshaevs@gmail.com>
2019-03-30 22:03:07 +0300

    Create a function that will check if the given letter is in the English alphabet.

--- HW_2_Lemeshaev_Stanislav/HW_2_Lemeshaev_Stanislav/main.m
+++ HW_2_Lemeshaev_Stanislav/HW_2_Lemeshaev_Stanislav/main.m
@@ -5,13 +5,27 @@
 //  Created by Станислав Лемешаев on 30/03/2019.
 //  Copyright © 2019 Станислав Лемешаев. All rights reserved.
 //
+//  1. Создать функцию, которая будет проверять, входит ли переданная буква в английский алфавит.
+//  2. Разделить метод Calculate (из практической задачи) на несколько методов (отдельно сложение, вычитание, умножение и деление).
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+// 1. Функция, которая проверяет, входит ли переданная буква в английский алфавит.
+BOOL isLetterEnglishABC(char c) {
+    if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))) {
+        return true;
+    }
+    else
+        return false;
+}
+
+// Главная программа
+int main() {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        printf("Enter your symbol: ");
+        char sym;
+        scanf("%s", &sym);
+        NSLog(@"%d", isLetterEnglishABC(sym));
     }
     return 0;
 }
