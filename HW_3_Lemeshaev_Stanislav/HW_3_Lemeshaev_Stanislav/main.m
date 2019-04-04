fc8d8d2d647ace5d54158779223aa1f9c79fa54e
slemeshaev <lemeshaevs@gmail.com>
2019-04-04 22:51:10 +0300

    Created files for Home work 3 Objective-C

---
+++ HW_3_Lemeshaev_Stanislav/HW_3_Lemeshaev_Stanislav/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HW_3_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 04/04/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
