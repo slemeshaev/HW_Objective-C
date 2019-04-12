30b915c3c323ab28587961a8dbca258532df0d36
slemeshaev <lemeshaevs@gmail.com>
2019-04-12 21:09:45 +0300

    Created files for Student

---
+++ HW_4_Lemeshaev_Stanislav/HW_4_Lemeshaev_Stanislav/Student.h
@@ -0,0 +1,17 @@
+//
+//  Student.h
+//  HW_4_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 10/04/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HW_4_Lemeshaev_Stanislav/HW_4_Lemeshaev_Stanislav/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  HW_4_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 10/04/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
