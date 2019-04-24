498ead9c20dcbe0cf7ecbbd89802b5d39d8ff14f
slemeshaev <lemeshaevs@gmail.com>
2019-04-24 10:22:03 +0300

    Created files Atithmetics

---
+++ HW_6_Lemeshaev_Stanislav/HW_6_Lemeshaev_Stanislav/Arithmetics.h
@@ -0,0 +1,17 @@
+//
+//  Arithmetics.h
+//  HW_6_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 24/04/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Arithmetics : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HW_6_Lemeshaev_Stanislav/HW_6_Lemeshaev_Stanislav/Arithmetics.m
@@ -0,0 +1,13 @@
+//
+//  Arithmetics.m
+//  HW_6_Lemeshaev_Stanislav
+//
+//  Created by Станислав Лемешаев on 24/04/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import "Arithmetics.h"
+
+@implementation Arithmetics
+
+@end
